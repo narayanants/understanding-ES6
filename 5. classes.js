78059d15b2bd9aa4a57dc2ebf4edf1f66be6aed1
Narayanan Ts <tsnarayanan7@gmail.com>
2017-11-01 15:19:37 +0530

    classes

--- 5. classes.js
+++ 5. classes.js
@@ -19,3 +19,37 @@ let bucky = new Person('Narayanan',24,5460);
 let vicky = new Person('Vicky',21,2350);
 bucky.displayWeight();
 vicky.displayWeight();
+
+
+class Police{
+    constructor(name,age,weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+
+    displayheight(){
+        console.olog()
+    }
+}
+
+
+
+
+
+class Police{
+    constructor(name,age,weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    displayheight(){
+        console.log(this.weight);
+    }
+}
+
+
+let narayanan = new Police('Gokul',23,5432);
+narayanan.displayheight();
