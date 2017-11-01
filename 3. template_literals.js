0269b9dbe3b302c17fcb135fb3ff32fd9d06f485
Narayanan Ts <tsnarayanan7@gmail.com>
2017-11-01 14:36:56 +0530

    template literals

---
+++ 3. template_literals.js
@@ -0,0 +1,27 @@
+/**
+ * Template Literals
+ */
+
+ "use strict";
+
+let name = 'Bucky';
+
+console.log('Favourite person is ' +  name + ' because he smells good.');
+console.log(`Favourite person is ${name} because he smells good.`);
+
+
+let a= 7;
+let b = 10;
+
+console.log(`Favourite person is ${a + b} because he smells good.`);
+console.log(`Favourite person is ${name} because
+he smells good.`);
+
+
+
+
+
+ 
+
+
+
